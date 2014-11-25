f3be19487a35d7552fd5e438828c491c23a3b87c
Dominik Schöni <dominik@dominikschoeni.ch>
2014-11-25 21:56:46 +0100

    fixes

--- src/android/StatusBarColor.java
+++ src/android/StatusBarColor.java
@@ -1,4 +1,4 @@
-package net.ekuwang.cordova.plugin.statusbar;
+package ch.theelysianfields.cordova.plugin.statusbar;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -6,24 +6,27 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.graphics.Color;
-import android.view.WindowManager;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
-import android.view.WindowManager.LayoutParams;
-import android.util.Log;
+import android.view.WindowManager;
 
 public class StatusBarColor extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException {
-	// grab the correct methods
 
 	if (action.equalsIgnoreCase("setColor")) {
 	    if (VERSION.SDK_INT > VERSION_CODES.KITKAT) {
+		final JSONArray finalargs = args;
 		cordova.getActivity().runOnUiThread(new Runnable() {
+		    @Override
 		    public void run() {
-			cordova.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-			cordova.getActivity().getWindow().setStatusBarColor(Color.rgb(args.getInt(0), args.getInt(1), args.getInt(2)));
+			try {
+			    cordova.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+			    cordova.getActivity().getWindow().setStatusBarColor(Color.rgb(finalargs.getInt(0), finalargs.getInt(1), finalargs.getInt(2)));
+			} catch (JSONException e) {
+			    // TODO: handle exception
+			}
 		    }
 		});
 		callback.success();
@@ -35,5 +38,6 @@ public class StatusBarColor extends CordovaPlugin {
 	    callback.error("Unknown Action: " + action);
 	    return false;
 	}
+
     }
 }
