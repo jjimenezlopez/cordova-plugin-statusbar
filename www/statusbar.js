e41e7c7f2af242e3cc2e8f120273c218b1954a31
Dominik Schöni <dominik@dominikschoeni.ch>
2014-11-25 21:53:28 +0100

    statusbar.js

--- www/statusbar.js
+++ www/statusbar.js
@@ -1,36 +1,12 @@
-function StatusbarTransparent () {
-	this.transparentFlag = false;
+function StatusBarColor () {
+	
 };
 
 /*
-* Enable translucent statusbar
+* set color of statusbar
 */
-StatusbarTransparent.prototype.enable = function (success, fail) {
-	if (this.transparentFlag == false) {
-		this.transparentFlag = true;
-		cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'enable', []);
-	}
+StatusBarColor.prototype.setColor = function (red, green, blue) {
+	cordova.exec(null, null, 'StatusBarColor', 'setColor', [red, green, blue]);
 };
 
-/*
-* Disable translucent statusbar
-*/
-StatusbarTransparent.prototype.disable = function (success, fail) {
-	if (this.transparentFlag == true) {
-		this.transparentFlag = false;
-		cordova.exec(success ? success : null, fail ? fail : null, 'StatusbarTransparent', 'disable', []);
-	}
-}
-
-/*
-*	Toggle the translucent statusbar
-*/
-StatusbarTransparent.prototype.toggle = function (success, fail) {
-	if (this.transparentFlag == true) {
-		this.disable(success, fail);
-	} else {
-		this.enable(success, fail);
-	}
-};
-
-module.exports = new StatusbarTransparent();
+module.exports = new StatusBarColor();
